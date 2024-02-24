9bed8c1fc5f2b74b091ee70749b0475a21266a5d
Ashutosh <ashutoshdatt777@gmail.com>
2024-02-24 15:51:13 +0530

    env variable updates

--- src/utilities/apiUrl.js
+++ src/utilities/apiUrl.js
@@ -1,10 +1,10 @@
 const API_URLS = {
     FETCH_CURRENT_WEATHER: (lat, long) =>
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.API_KEY}&units=metric`,
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}&units=metric`,
     FETCH_FORECAST_DETAILS: (lat, long) =>
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${process.env.API_KEY}&units=metric`,
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}&units=metric`,
     FETCH_CITIES_LIST: (searchValue) =>
-        `https://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=5&appid=${process.env.API_KEY}`,
+        `https://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=5&appid=${process.env.REACT_APP_API_KEY}`,
 };
 
 export default API_URLS;
